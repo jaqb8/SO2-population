c232b58c60d5ef7177a97baec6ca075c40f0acf2
Jakub Aniszewski <qba.anisz@gmail.com>
2020-05-03 18:10:19 +0200

    Person & Interface thread target functions

--- main.py
+++ main.py
@@ -1,70 +1,50 @@
 from interface import Interface
+from person import Person
 import threading
 import time
 
 interface_lock = threading.Lock()
 alive = 1
 interface = Interface()
+interface.init_interface()
 
 
-class PersonThread(threading.Thread):
+def person_thread(person, person_number):
+    global alive
+    alive += 1
 
-    def __init__(self, person, person_number):
-        threading.Thread.__init__(self)
-        self.person = person
-        self.person_number = person_number
+    while not interface.is_curses_initialized:
+        time.sleep(.1)
 
-    def run(self):
-        global alive
-        alive += 1
+    while person.alive:
+        with interface_lock:
+            interface.print_person(person_number, person)
 
-        while not interface.is_curses_initialized:
-            time.sleep(0.1)
+        time.sleep(1)
+        person.increase_age()
 
-        while self.person.alive:
-            interface_lock.acquire()
-            interface.print_person(self.person_number, self.person)
+        if person.age >= person.life_expectancy:
+            person.kill()
 
-            interface_lock.release()
+    with interface_lock:
+        interface.print_person_death(person_number, person)
 
-            time.sleep(1)
-
-            self.person.increase_age()
-
-            if self.person.age >= self.person.life_expectancy:
-                self.person.kill()
-
-        interface_lock.acquire()
-        interface.print_person_death(self.person_number, self.person)
-        interface_lock.release()
-        alive -= 1
-
-        time.sleep(0.1)
+    alive -= 1
+    time.sleep(.1)
 
 
-class InterfaceThread(threading.Thread):
-
-    def run(self):
-        interface_lock.acquire()
-
+def interface_thread():
+    with interface_lock:
         interface.init_interface()
         interface.print_info()
 
-        interface_lock.release()
-
-        while alive > 0:
-            interface_lock.acquire()
+    global alive
+    while alive > 0:
+        with interface_lock:
             interface.refresh_display()
-            interface_lock.release()
+        time.sleep(1)
 
-            time.sleep(1)
-
-        interface_lock.acquire()
+    with interface_lock:
         interface.end_curses()
-        interface_lock.release()
-        time.sleep(0.1)
 
-
-interface_lock.acquire()
-ui = InterfaceThread()
-ui.start()
+    time.sleep(.1)
